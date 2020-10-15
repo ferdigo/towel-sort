9069e33e052485f136d0d73d62149000775645f8
Roman Zhuk <ferdigo999@gmail.com>
2020-10-15 18:27:47 +0300

    feat: basic functionality

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+matrix.forEach((item, index, array) => {
+    if(index%2 !== 0){
+        item.sort((a,b) => a-b)
+    }
 
-return matrix.flat(2).sort((a,b) => a-b)
+});
+    
+
+return matrix.flat(2)//.sort((a,b) => a-b)
 
 }
